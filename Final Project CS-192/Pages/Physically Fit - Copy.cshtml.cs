9010d08271373e014458677365391ca3339a9655
dlc11 <davidlc1229@gmail.com>
2023-05-08 16:27:37 -0700

    Add project files.

---
+++ Final Project CS-192/Pages/Physically Fit - Copy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Final_Project_CS_192.Pages
+{
+    public class Newpage : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public Newpage(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Final Project CS-192/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Final_Project_CS_192.Pages
+{
+    public class Privacypage : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public Privacypage(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
